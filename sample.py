e6acbdb30b19f67c1ec5475a95375824818fc81f
crophix <dleblanc612@gmail.com>
2012-11-19 16:48:40 -0800

    Added sample.py code stub

---
+++ sample.py
@@ -0,0 +1,9 @@
+#Author Name
+
+def funcName(input1, input2):
+	""" Brief description of the function.
+	
+	Addition description of the function if it is needed"""
+	#function code
+	return None	
+	
