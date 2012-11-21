4734a03d9816bc57fffb999eed55b8278f267a4f
crophix <dleblanc612@gmail.com>
2012-11-21 10:29:28 -0800

    Added a file to contain test data for later testing

---
+++ testData.py
@@ -0,0 +1,10 @@
+# This File is to contain sample data to use as inputs when 
+#	testing the individual functions.  By keeping all of the
+#	data here we can hopefully minimize the work.
+# Data should keep to the naming conventions listed.  A list
+#	containing all valid data should be maintained here to 
+#	simplify the automated testing.
+
+# Sample valid CacheObj, naming convention: "cov#"
+
+# Sample invalid CacheObj, naming convention: "coi#"
