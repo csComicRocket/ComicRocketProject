8879845c46b19806e9a4c992b9c000c8e7e88954
Eric Smith <easmith@pdx.edu>
2012-11-25 20:29:47 -0800

    Added HistoryCheck & NewComicCheck stubs

---
+++ testHistoryCheck.py
@@ -0,0 +1,10 @@
+#Eric Smith
+
+def testHistoryCheck(comicURL):
+	""" Checks to see if any older posts in the comic
+            have changed.
+
+            Fetches Cache and Web version of the comic page,
+            compares the two and notifies about differences."""
+	#function code
+	return None	
---
+++ testNewComicCheck.py
@@ -0,0 +1,9 @@
+#Eric Smith
+
+def testNewComicCheck(comicID):
+	""" Checks to see if the comic has updated.
+
+            Fetches Cache and Web version of the comic page,
+            compares the two and notifies about differences."""
+	#function code
+	return None	
