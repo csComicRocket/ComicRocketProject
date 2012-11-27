02905ecbda6fadb555ed3bd5e5861761f5aeb23d
Suzanna Kangas <skangas@ruby.cat.pdx.edu>
2012-11-27 11:05:47 -0800

    Added credit to author of tree structure. Let me know if this is sufficient or if it needs to be reworded or reformatted. Thanks.

--- utilities.py
+++ utilities.py
@@ -1,3 +1,10 @@
+# Code for pageTree, pageNode, and sanitizeId function originated from
+# Brett Alistair Kromkamp - brettkromkamp@gmail.com
+# www.quesucede.com
+# Code has been modified. Class, variable, and function names have been
+# changed from Brett's original code. Data types have been changed and
+# new data members and corresponding methods [will be] added to code.
+# Tree structure is kept from original code. [might be changed, but doubt it.]
 
 def sanitizeId(id):
     return id.strip().replace(" ", "")
