a5137e0cc0f2751d1fe84612b7a77d7726f20451
crophix <dleblanc612@gmail.com>
2013-01-29 23:25:10 -0800

    Worked on crFunctions and runTest

--- RocketBooster/F1Engine/crFunctions.py
+++ RocketBooster/F1Engine/crFunctions.py
@@ -8,10 +8,46 @@ unlock predictor data
 set update schedule (comic id, list of day/hour tuples)
 """
 
-def resetPredHist(argList):
-    """Reset the predictor history to default settings."""
+def resetPredData(argList):
+    """Reset the predictor history to default settings.
+    
+    argList should be a comic id"""
     try:
         comicId = int(argList)
     except ValueError:
         return
     #Do stuff to reset predictor history of comicId
+    
+def lockPredData(argList):
+    """Lock a comics expected update schedule.
+    
+    argList should be a comic id
+    Use this if a comic is going on hiatus and you want to avoid the predictor
+    data losing accuracy."""
+    try:
+        comicId = int(argList)
+    except ValueError:
+        return
+    #Do stuff to lock the predictor data of comicId
+    
+def unlockPredData(argList):
+    """Unlock a comics expected update schedule.
+    
+    argList should be a comic id"""
+    try:
+        comicId = int(argList)
+    except ValueError:
+        return
+    #Do stuff to unlock the predictor data of comicId
+    
+def setUpdateSchedule(argList):
+    """Set a comics update schedule to provided times.
+    
+    argList should be a comicId and a list of (day,hour) tuples"""
+    try:
+        argList = argList.partition(',')
+        comicId = int(argList[0])
+        schedule = argList[2] #still needs work
+    except ValueError:
+        return
+    #Update the predictor history using the provided list
--- RocketBooster/F1Engine/tempAPITester.py
+++ RocketBooster/F1Engine/tempAPITester.py
@@ -10,8 +10,29 @@ def callMethod(string):
         return
     methodToCall(args[0])
     
+def runTests():
+    tests = []
+    try:
+        callMethod("resetPredData(1101)")
+        tests.append(("callMethod",1,True))
+    except:
+        tests.append(("callMethod",1,False))
+    try:
+        callMethod("resetPredData(1,2,3)")
+        tests.append(("callMethod",2,True))
+    except:
+        tests.append(("callMethod",2,False))
+    try:
+        callMethod("invalidName()")
+        tests.append(("callMethod",3,True))
+    except:
+        tests.append(("callMethod",3,False))
+    try:
+        callMethod("invalidFormat")
+        tests.append(("callMethod",4,True))
+    except:
+        tests.append(("callMethod",4,False))
+    return tests
+    
 if __name__ == '__main__':
-    callMethod("resetPredHist(1101)")
-    callMethod("resetPredHist(1,2,3)")
-    callMethod("invalidName()")
-    callMethod("invalidName2")
+    runTests()
