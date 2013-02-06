792721dc89fee4c9c462a69913aa637fc789912e
crophix <dleblanc612@gmail.com>
2013-02-05 21:02:30 -0800

    Added some things to cache and sheduler

--- RocketBooster/Scheduler.py
+++ RocketBooster/Scheduler.py
@@ -1,12 +1,14 @@
-#Fredrik Fostvedt and Daniel Leblanc
+#Daniel Leblanc
 
 import time
 import os
 import threading
 import Predictor
+import F1Engine.checkComic
 
 SITELIMIT = 20
 PAGELIMIT = 20
+SECONDS = 60
 
 class ComicList:
     """Represents a current list of pages in a comic that need to be checked.
@@ -25,7 +27,7 @@ class ComicList:
         with open(self.histData) as f:
             data = f.readline()
             data = data.strip()
-            self.lastComplete = time.strptime(data, gmtime())
+            self.lastComplete = time.strptime(data, time.gmtime())
             data = f.readline()
             data = data.strip()
             self.current = int(data)
@@ -33,21 +35,22 @@ class ComicList:
 		
     def getNext(self):
         """Returns the next url to be checked or None if the list has been completed"""
+        urlList = []
+        end = False
         with open(self.urls) as f:
             f.seek(self.current)
-            item = f.readline()
+            for i in range(SITELIMIT):
+                urlList.append(f.readline().strip())
+                if not urlList[i]:
+                    end = True
             self.current = f.tell()
-        if not item:
+        if end:
             self.current = 0
             self.lastComplete = time.gmtime()
-            with open(self.histData, 'w+') as f:
-                f.write(time.strftime(time.gmtime(), gmtime()) + '\n')
-                f.write('0/n')
-            return None
         with open(self.histData, 'w+') as f:
-            f.write(time.strftime(self.lastComplete, gmtime()) + '\n')
-            f.write(str(self.current) + '/n')
-        return item.strip()
+            f.write(time.strftime(self.lastComplete, time.gmtime()) + '\n')
+            f.write(str(self.current))
+        return urlList
 		
 class HistoryList:
     """Contains the list of all comics to be checked.
@@ -76,11 +79,12 @@ class HistoryList:
 	
     def getComic(self):
         """Retrieves a comic from the list"""
-        item = self.comics[0].getNext()
-        while item == None:
+        urlList = self.comics[0].getNext()
+        while len(urlList) == 0:
             self.waiting.append(self.comics.pop(0))
-            item = self.comics[0].getNext()
-        return item
+            urlList = self.comics[0].getNext()
+        self.waiting.append(self.comics.pop(0))
+        return urlList
 		
     def recoverWaiting(self):
         """Adds the waiting comics back into the list."""
@@ -91,22 +95,29 @@ class HistoryList:
 def scheduler():
     """Checks the new comics expected in each hour block and the archived comics"""
     global histComics
+    Predictor.scanDirectories()
     histComics = HistoryList(os.listdir('Cache/cacheInfo/'))
     currentTime = time.gmtime().tm_wday, time.gmtime().tm_hour
-    t = time.Timer((60-time.gmtime().tm_minute)*60, hourlyEvents)
+    t = time.Timer((60-time.gmtime().tm_minute)*SECONDS, hourlyEvents)
     t.start()
     while (True):
-        #check histComics.getComic()
-        pass
+        urlList = histComics.getComic()
+        for url in urlList:
+            F1Engine.checkComics.histCheck(url)
         
 def hourlyEvents():
     global histComics
     currentTime = time.gmtime().tm_wday, time.gmtime().tm_hour
     histComics.recoverWaiting()
-    for comicId in Predictor.getCheckComicIds(currentTime):
-        #if newComicCheck(comicId), call predictor.update(comicId)
-        pass
-    t = time.Timer(3600, hourlyEvents)
+    for comicId in Predictor.getHourList(currentTime):
+        directory = "../../cache/predictorInfo/" + str(comicId) + "/last3Pages.txt"
+        urls = []
+        with open(directory) as f:
+            for line in f:
+                urls.append(line.strip())
+        for url in urls:
+            F1Engine.checkComics.newComic(url)
+    t = time.Timer((60-time.gmtime().tm_minute)*SECONDS, hourlyEvents)
     t.start()
 		
 def runTests():
