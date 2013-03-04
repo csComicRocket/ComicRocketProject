3a5f503ae21784b0d0725b999d84652651bf6acd
crophix <dleblanc612@gmail.com>
2013-03-04 14:46:09 -0800

    fixed issue with http listener

--- RocketBooster/F1Engine/fetchHTTP.py
+++ RocketBooster/F1Engine/fetchHTTP.py
@@ -3,11 +3,11 @@ import J2Engine.fetchWeb
 from J2Engine.cache import Cache
 
 def fetchHTTP(url, comicID, imgs=None):
-	""" Checks cache for URL, if none, call fetchWeb. Returns PageTree"""
+    """ Checks cache for URL, if none, call fetchWeb. Returns PageTree"""
     cache = Cache()
-	pt = cache.fetchCache(url)
-	if (pt is None):
-		pt = fetchWeb.fetchWeb(url, comicID)	
+    pt = cache.fetchCache(url)
+    if (pt is None):
+        pt = fetchWeb.fetchWeb(url, comicID)    
 
 if __name__ == "main":
-	fetchHTTP()
+    fetchHTTP()
