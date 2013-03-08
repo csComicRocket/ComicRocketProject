a55bebf2f7c6a38464c920628e40b68acdb639ee
crophix <dleblanc612@gmail.com>
2013-03-08 11:10:43 -0800

    testing compare

--- RocketBooster/F1Engine/J2Engine/compare.py
+++ RocketBooster/F1Engine/J2Engine/compare.py
@@ -2,13 +2,13 @@
 
 from LunarModule.pageTree import *
 
-def compare(oldPage,newPage):
+def hashCompare(oldPage,newPage):
     if(oldPage.getHash() == newPage.getHash()):
         return True
     else:
         return False
 
-def linkCompare(oldPage,newPage):
+def compare(oldPage,newPage):
     if len(oldPage.links) == len(newPage.links):
         for i in xrange(len(oldPage.links)):
             if i not in oldPage.blacklist:
