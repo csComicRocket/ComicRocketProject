246e1309dfb7fa2b245461d8532121aad0c00b48
crophix <dleblanc612@gmail.com>
2013-03-08 12:10:57 -0800

    testing compare

--- RocketBooster/F1Engine/J2Engine/fetchWeb.py
+++ RocketBooster/F1Engine/J2Engine/fetchWeb.py
@@ -59,8 +59,7 @@ def fetchWeb(url, comicID, imgs=None):
         pageStr    = soup.prettify()
         headerDict = rsp.info()
         url = rsp.geturl()
-        print "stuff: " + str(len(pageStr))
-
+       
         tree = fillNode(tree, pageStr, headerDict, url, comicID, nodeID, None)       # Fill root node
 
         links = []
